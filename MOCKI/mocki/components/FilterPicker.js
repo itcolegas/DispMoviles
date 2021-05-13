dd11a6e2f78df4a5c19f785fb61eb2cf88e852d1
ccejudo <cristopher.cm1999@gmail.com>
2021-05-12 22:40:46 -0500

    Agregando componente FilterPicker para los filtros de los problemas

---
+++ MOCKI/mocki/components/FilterPicker.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import RNPickerSelect from 'react-native-picker-select';
+
+export default function FilterPicker(props) {
+    let placeholder = props.placeholder? props.placeholder : 'Select';
+    let items = props.items? props.items : [];
+
+    const getItems = (items) => {
+        return items.map(item => {
+            return { label: item, value: item.toLowerCase(), key: item.toLowerCase() }
+        })
+    }
+
+    return (
+        <>
+        { props.setSelected ?
+            <RNPickerSelect
+                placeholder={{label: placeholder, value: null, color: '#9FA0B9'}}
+                onValueChange={(value) => console.log(value)}
+                items={getItems(items)}
+            />: null
+        }
+        </>
+    )
+}
