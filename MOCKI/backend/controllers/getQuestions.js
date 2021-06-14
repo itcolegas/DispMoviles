51571fca403a1fe436bda9deb8e31bbe6ec6a451
ccejudo <cristopher.cm1999@gmail.com>
2021-06-14 10:39:58 -0500

    Adding routes to backend

---
+++ MOCKI/backend/controllers/getQuestions.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient
+
+const getQuestions = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URL, { useUnifiedTopology: true })
+
+    console.log('getQuestions')
+    try {
+        client.connect( (err) => {
+            if (err) {
+                console.log(err)
+                res.send(err)
+            }
+            const db =  client.db('mocki')
+
+            db.collection('questions').find().toArray(function(err, result) {
+                if (err) {
+                    console.log(err)
+                    res.send(err)
+                }
+                console.log(result)
+                res.json({questions: result})
+            })
+        })
+    } catch (err) {
+        console.log(err);
+        res.send(err)
+    } finally {
+        client.close()
+    }
+}
+
+module.exports = { getQuestions }
--- MOCKI/backend/routes/index.js
+++ MOCKI/backend/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const { getQuestions } = require('../controllers')
+
+router.get('/get-questions', getQuestions)
+
 module.exports = router
